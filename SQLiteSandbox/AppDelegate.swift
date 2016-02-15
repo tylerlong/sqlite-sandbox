29163c13d9650a80440146d830bcde27392c40d7
Tyler Long <tyler4long@gmail.com>
2016-02-15 22:21:40 +0800

    Initial Commit

---
+++ SQLiteSandbox/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SQLiteSandbox
+//
+//  Created by Tyler Long on 2/15/16.
+//  Copyright © 2016 Tylingsoft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
